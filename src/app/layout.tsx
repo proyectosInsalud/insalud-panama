9ec38fdf608b0232f3b0f5842807289d0d2ffe3a
proyectosInsalud <proyectos.ti@insalud.pe>
2026-02-17 11:00:12 -0500

    Add Google Tag Manager integration to layout component
    - Included Google Tag Manager script for enhanced tracking and analytics.
    - Added noscript fallback for users with JavaScript disabled.
    - Updated layout structure to accommodate the new tracking features.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Nunito } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import { Toaster } from "@/shared/ui/Toaster";
 
@@ -23,7 +24,30 @@ export default function RootLayout({
 }) {
   return (
     <html lang="es">
+      <head>
+      {/* Google Tag Manager */}
+      <Script
+          id="gtm"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-N7RFRHHG');`,
+          }}
+        />
+      {/*  End Google Tag Manager */}
+      </head>
       <body className={`${nunito.variable} font-sans antialiased`}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N7RFRHHG"
+          height="0" width="0"
+          style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         {children}
         <Toaster />
       </body>
